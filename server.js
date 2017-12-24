3065a45a6478e7394006992d56f209aa8d5a469f
Andrew Cox <drew.r.cox@gmail.com>
2017-12-23 22:28:58 -0500

    Fix config variables and implement cors

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require("express");
 const request = require("request");
 const cors = require("cors");
 const http = require("http");
-const {API_KEY} = require("./config");
 const {CLIENT_ORIGIN} = require("./config");
 const app = express();
-const PORT = process.env.PORT || 8000;
 
 app.use(express.static("./"));
 
@@ -13,18 +11,19 @@ app.use(
   cors({ origin: CLIENT_ORIGIN })
 );
 
+app.get("/data/:query", (req, res) => {
+  let url = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${req.params.query}&apikey=S3H1N4O2750Y09RN`;
+  request.get({ url: url, headers: {"content-type": "application/json"} }).pipe(res);
+});
+
 app.get("/headlines/:query", (req, res) => {
   let url = `https://finance.yahoo.com/rss/headline?s=${req.params.query}&region=US&lang=en-US`;
   request.get({ url: url, headers: {"content-type": "text/xml"} }).pipe(res);
-});
-
-app.get("/data/:query", (req, res) => {
-  let url = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${req.params.query}&apikey=${API_KEY}`;
-  request.get({ url: url, headers: {"content-type": "application/json"} }).pipe(res);
 });
 
 app.use("*", function (req, res) {
   return res.status(404).json({ message: "Not Found" });
 });
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => { console.log(`Listening on port ${PORT}`); });
